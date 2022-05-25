265ee0feb777e5897fae86d95ec048ccc8ac24da
Karthik Vijay <kv52274@gmail.com>
2022-05-25 15:21:15 +0530

    Update stat.c

--- stat.c
+++ stat.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 
 int main()
 {
@@ -10,9 +8,10 @@ int main()
   
   char path[100];
   
-  printf("Enter the location of the file to be checked:");
+  printf("Enter the location of file to be checked:");
   scanf("%s",path);
   printf("\n");
+  
   int a = stat(path, &file); 
   
   if(a==-1)
@@ -21,6 +20,7 @@ int main()
   {
      printf("ID of the given file is = %o\n", file.st_uid);
      printf("Size of the given file is = %ld bytes\n", file.st_size);
+     printf("Last accessed time = %s", ctime(&file.st_atime));
      printf("Last modified time = %s\n", ctime(&file.st_mtime));
      
      printf("User File Permissions:");
